723592998f9a20bf3212a0bfa3ed6b68e3e5ed99
oliolioli <oli.meier@bluewin.ch>
2024-04-30 23:08:07 +0200

    Added spheres, lightning and physical action

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 import * as THREE from 'three';
 import {OrbitControls} from 'three/examples/jsm/controls/OrbitControls.js';
+import * as dat from 'dat.gui';
 
 const renderer = new THREE.WebGLRenderer();
 
@@ -27,27 +28,81 @@ orbit.update();
 
 
 const boxGeometry = new THREE.BoxGeometry();
-const boxMaterial = new THREE.MeshBasicMaterial({color: 0xFFFFFF});
+const boxMaterial = new THREE.MeshBasicMaterial({color: 0x00AAFF});
 const box = new THREE.Mesh(boxGeometry, boxMaterial);
 
 scene.add(box);
 
-const planeGeometry = new THREE.PlaneGeometry(40, 40);
-const planeMaterial = new THREE.MeshBasicMaterial({color: 0x2233FF});
+const planeGeometry = new THREE.PlaneGeometry(30, 30);
+const planeMaterial = new THREE.MeshBasicMaterial({
+  color: 0xFFFFFF,
+  side: THREE.DoubleSide
+});
+  
 const plane = new THREE.Mesh(planeGeometry, planeMaterial);
 
 scene.add(plane);
 
-plane.rotation.x = 0.5 * Math.PI;
+plane.rotation.x = -0.5 * Math.PI;
 
-const gridHelper = new THREE.GridHelper(40);
+const gridHelper = new THREE.GridHelper(30,100);
 scene.add(gridHelper);
+
+const sphereGeometry = new THREE.SphereGeometry(4, 50, 50);
+const sphereMaterial = new THREE.MeshStandardMaterial({
+  color: 0x0000FF,
+  wireframe: true
+});
+const sphere = new THREE.Mesh(sphereGeometry,sphereMaterial);
+scene.add(sphere);
+
+sphere.position.set(-10, 10, 10);
+
+const gui = new dat.GUI();
+
+const ambientLight = new THREE.AmbientLight(0x333333);
+scene.add(ambientLight);
+
+const directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.8);
+scene.add(directionalLight);
+directionalLight.position.set(-30, 50, 0);
+
+const dLightHelper = new THREE.DirectionalLightHelper(directionalLight, 5);
+scene.add(dLightHelper);
+
+const options = {
+  sphereColor: '#ffea00',
+  planeColor: '#ffffff',
+  wireframe: true,
+  speed: 0.01
+};
+
+gui.addColor(options, 'sphereColor').onChange(function(e){
+  sphere.material.color.set(e);
+});
+
+gui.addColor(options, 'planeColor').onChange(function(e){
+  plane.material.color.set(e);
+})
+
+gui.add(options, 'wireframe').onChange(function(e){
+  sphere.material.wireframe = e;
+});
+
+gui.add(options, 'speed', 0, 0.1);
+
+let step = 0;
 
 
 
 function animate() {
   box.rotation.x += 0.01;
   box.rotation.y += 0.01;
+
+  step += options.speed;
+
+  sphere.position.y = 10 * Math.abs(Math.sin(step));
+
   renderer.render(scene, camera);
 }
 
